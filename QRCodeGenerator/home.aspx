aa32e813d65dbf67e5acfe281bed538e976f8dc9
Jirasin Jirasakul <jirasin@genxas.com>
2018-10-21 17:24:30 +0700

    first

---
+++ QRCodeGenerator/home.aspx
@@ -0,0 +1,20 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="home.aspx.cs" Inherits="QRCodeGenerator.home" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        Link : <asp:TextBox ID="txtCode" runat="server"></asp:TextBox><br /><br />
+        Width : <asp:TextBox ID="txtWidth" runat="server"></asp:TextBox><br /><br />
+        Height : <asp:TextBox ID="txtHeight" runat="server"></asp:TextBox><br /><br />
+        <asp:Button ID="btnGenerate" runat="server" Text="Generate" OnClick="btnGenerate_Click" />
+        <br /><br />
+        <asp:Label ID="lblMsg" runat="server" Text=""></asp:Label>
+    </div>
+    </form>
+</body>
+</html>
---
+++ QRCodeGenerator/home.aspx.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Net;
+using System.IO;
+
+namespace QRCodeGenerator
+{
+    public partial class home : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnGenerate_Click(object sender, EventArgs e)
+        {
+            List<RunnerInfo> infos = new List<RunnerInfo>();
+            using (RunningDatabaseDataContext r = new RunningDatabaseDataContext())
+            {
+                infos = r.RunnerInfos.ToList();
+            }
+
+            foreach (var info in infos)
+            {
+                WebResponse response = default(WebResponse);
+                Stream remoteStream = default(Stream);
+                StreamReader readStream = default(StreamReader);
+                var text = $"http://runningqrapi.azurewebsites.net/RunningTimes/RecordByScanView?runnerIdentification={info.RunnerBIB}";
+                var url = string.Format("http://chart.apis.google.com/chart?cht=qr&chs={1}x{2}&chl={0}", text, 500, 500);
+                WebRequest request = WebRequest.Create(url);
+                response = request.GetResponse();
+                remoteStream = response.GetResponseStream();
+                readStream = new StreamReader(remoteStream);
+                System.Drawing.Image img = System.Drawing.Image.FromStream(remoteStream);
+                img.Save($"D:/Projects/MillionCreation/Participants/QRCodes/{ info.RunnerBIB }.png");
+                response.Close();
+                remoteStream.Close();
+                readStream.Close();
+            }
+        }
+    }
+}
